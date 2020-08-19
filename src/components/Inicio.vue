993300297e3551cc2cecc75372ceeb147fcdfdb7
Solst1tium <solange.leiva@mayor.cl>
2020-08-18 21:21:00 -0400

    navbar terminado

---
+++ src/components/Inicio.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        
+        <h1>32bits</h1>
+        <h2>Juegos de Pc Y Consolas</h2>        
+        Acá va tu contenido
+    </div>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+}
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+}
+</script>
